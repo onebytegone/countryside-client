2d7f190fd37bdd3ef1e5dfdd5a81a0c75185ab9c
Ethan Smith <ethan@onebytegone.com>
2015-03-08 08:26:18 -0400

    Setup basic page. Using requirejs

---
+++ src/main.js
@@ -0,0 +1,8 @@
+require.config({
+    baseUrl: 'src',
+    paths: {
+        jquery: 'thirdparty/jquery-1.11.2.min',
+        underscore: 'thirdparty/underscore-min',
+        backbone: 'thirdparty/backbone'
+    }
+});
